5328b371a97c69acd75af80d666d445c84def1bb
Mario <mario@hfrocks.com>
2024-02-22 14:52:40 -0700

    use memory store

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const { connectToMongoDB } = require('./db/index');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-const session = require('cookie-session');
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
 const passport = require('passport');
 require('./passport');
 require('dotenv').config();
@@ -17,6 +18,10 @@ app
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
     .use(session({
+        cookie: { maxAge: 86400000 },
+        store: new MemoryStore({
+          checkPeriod: 86400000 // prune expired entries every 24h
+        }),
         secret: process.env.SECRET,
         resave: false,
         saveUninitialized: false
